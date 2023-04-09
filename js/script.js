c288b7ee8af690ea1f35daba4b6df765d451003a
Renditionzz <99329650+Renditionzz@users.noreply.github.com>
2023-04-09 00:40:40 -0700

    Create playRound()

--- js/script.js
+++ js/script.js
@@ -13,3 +13,70 @@ function getComputerChoice(){
     const choices = ["Rock", "Paper", "Scissors"];
     return choices[randomNumber];
 }
+
+/*
+    Create a function that lets the player a single round of Rock Paper Scissors.
+
+    playRound(playerSelection, computerSelection)
+        if player picks rock
+            if computer picks rock
+                tie
+            if computer picks scissors
+                player wins
+            if computer picks paper
+                computer wins
+        else if player picks paper
+            if computer picks rock
+                player wins
+            if computer picks scissors
+                computer wins
+            if computer picks paper
+                tie
+        else if player picks scissors
+            if computer picks rock
+                computer wins
+            else if computer picks paper
+                player wins
+            else if computer picks scissors
+                tie
+        else
+            invalid answer, try again
+        
+*/
+
+function playRound(playerSelection, computerSelection){
+    const player = playerSelection.toLowerCase();
+    let result;
+    if (player == "rock"){
+        if (computerSelection == "Rock"){
+            result = "Tie!";
+        } else if (computerSelection == "Paper"){
+            result = "You lose! Paper beats Rock!";
+        } else {
+            result = "You win! Rock beats Scissors!";
+        }
+    } else if (player == "paper"){
+        if (computerSelection == "Rock"){
+            result = "You win! Paper beats Rock!"
+        } else if (computerSelection == "Paper"){
+            result = "Tie!";
+        } else {
+            result = "You lose! Scissors beats Paper!"
+        }
+    } else if (player == "scissors"){
+        if (computerSelection == "Rock"){
+            result = "You lose! Rock beats Scissors!"
+        } else if (computerSelection == "Paper"){
+            result = "You win! Scissors beats Paper!"
+        } else {
+            result = "Tie!";
+        }
+    } else {
+        result = "Invalid, please try again!";
+    }
+    return result;
+}
+
+const player = "Rock";
+const computer = getComputerChoice();
+playRound(player, computer);
